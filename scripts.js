0dca0b8710ccfb5b03922eb42981e483f94d8476
wambete <wambetesoita@gmail.com>
2019-02-21 15:02:30 +0200

    babelrc removed

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Get DOM elements
+//Get DOM elements
 const generateButton = document.getElementById('generate-button');
 const postTitle = document.getElementById('post-title');
 const postId = document.getElementById('post-id');
